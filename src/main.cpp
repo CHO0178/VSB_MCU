6f1cf4cdf975621d3cdde6690d9e834e610daa7c
CHO0178 <115779219+CHO0178@users.noreply.github.com>
2026-06-07 21:27:48 +0200

    change main from C to CPP

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,22 @@
 /*
- * Name: main.c
+ * Name: main.cpp
  * Author: You
  *
  * This is a stub. Write your code here.
  *
  */
 
-#include "wdog.h"
+extern "C" { // Allow C functions to be called from C++ code
+	#include "wdog.h"
+	#include "littleHelper.h"
+}
 
 __attribute__ ((weak)) int main(void)
 {
 	wdog_init(WDOG_CONF_DIS);
 
 	while (1) {
-
+		wdog_refresh();
 	}
 
 	return 0;
